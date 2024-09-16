bb9d8c28f6381f0fb156fe2512a3da4191786cda
Frederic Chien <fred_chien@yahoo.com.br>
2024-09-16 15:06:50 -0300

    fix: adjust navbar

--- nextjs-course/src/components/Navbar/Navbar.tsx
+++ nextjs-course/src/components/Navbar/Navbar.tsx
@@ -1,30 +1,48 @@
 import { HomeIcon, GameIcon, TopIcon, WalkIcon, UserIcon } from "@/components";
 
+const NavbarList = ({children, className}) => {
+  return (
+    <ul className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}>
+      {children}
+    </ul>
+  );
+}
+
+const NavbarListItem = ({children, className}) => {
+  return (
+    <li className={`my-2 rounded-lg px-2 bg-transparent hover:bg-indigo-400/40 hover:text-slate-100 cursor-pointer flex gap-2 items-center ${className}`}>
+      {children}
+    </li>
+  );
+}
+
+
+
 export const NavBar = () =>{
     return(
-        <nav className="flex flex-col gap-4 h-screen bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 text-slate-300">
-          <div>
-            <img src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png" alt="Logo Emerson Broga" className="max-w-full p-2"/>
+        <nav className="flex flex-col h-screen bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/80 w-72 p-2 text-slate-300">
+          <div className="flex items-center justify-center my-4">
+            <img src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png" alt="Logo Emerson Broga" className="w-auto h-12 p-2"/>
           </div>
-          <ul className="flex-grow my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-            <li className="my-2 rounded-lg px-2 bg-transparent hover:bg-slate-800 hover:text-slate-100 cursor-pointer flex gap-2 items-center">
+          <NavbarList className={'flex-grow'}>
+            <NavbarListItem>
               <HomeIcon className="h-4 w-4"/> Home
-            </li>
-            <li className="my-2 rounded-lg px-2 bg-transparent hover:bg-slate-800 hover:text-slate-100 cursor-pointer flex gap-2 items-center">
+            </NavbarListItem>
+            <NavbarListItem>
               <GameIcon className="h-4 w-4"/> Games
-            </li>
-            <li className="my-2 rounded-lg px-2 bg-transparent hover:bg-slate-800 hover:text-slate-100 cursor-pointer flex gap-2 items-center">
+            </NavbarListItem>
+            <NavbarListItem>
               <TopIcon className="h-4 w-4"/> Top 10
-            </li>
-            <li className="my-2 rounded-lg px-2 bg-transparent hover:bg-slate-800 hover:text-slate-100 cursor-pointer flex gap-2 items-center">
+            </NavbarListItem>
+            <NavbarListItem>
               <WalkIcon className="h-4 w-4"/> Walkthroughs
-            </li>
-          </ul>
-          <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-            <li className="my-2 rounded-lg px-2 bg-transparent hover:bg-slate-800 hover:text-slate-100 cursor-pointer flex gap-2 items-center">
+            </NavbarListItem>
+          </NavbarList>
+          <NavbarList>
+            <NavbarListItem>
               <UserIcon className="h-4 w-4"/> User
-            </li>
-          </ul>
+            </NavbarListItem>
+          </NavbarList>
         </nav>
     )
 }
