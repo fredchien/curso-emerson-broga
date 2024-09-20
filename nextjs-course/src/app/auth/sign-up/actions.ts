5cc56a071281caad70fad1699a17207a9b87fbc7
Frederic Chien <fred_chien@yahoo.com.br>
2024-09-20 10:57:07 -0300

    feat: add validation server

--- nextjs-course/src/app/auth/sign-up/actions.ts
+++ nextjs-course/src/app/auth/sign-up/actions.ts
@@ -2,7 +2,67 @@
 
 import UsersServices from "@/services/Users";
 
+export type SignUpError = {
+    name?: string,
+    email?: string,
+    password?: string,
+    passwordConfirmation?: string,
+}
+
+export type SignUpState = {
+    isValid?: boolean,
+    errors: SignUpError
+}
+
+const validateSignUpForm = (formdata: FormData) => {
+    const errors: SignUpError = {
+        name: undefined,
+        email: undefined,
+        password: undefined,
+        passwordConfirmation: undefined,
+    }
+
+    const name = String(formdata.get("name"));
+    const email = String(formdata.get("email"));
+    const password = String(formdata.get("password"));
+    const passwordConfirmation = String(formdata.get("passwordConfirmation"));
+
+    try {
+        if(!name){
+            errors.name = "Name is required";
+        }
+
+        if(!email){
+            errors.email = "Emails is required";
+        }
+
+        if(!email.includes('@')){
+            errors.email = "Emails is not valid";
+        }
+
+        if(password.length < 10){
+            errors.password = "Password should have 10 chars";
+        }
+
+        if(!password || password !== passwordConfirmation){
+            errors.passwordConfirmation = "Password Confirmation doesnt match";
+        }
+
+        const isValid = Object.values(errors).every((value) => value === undefined);
+        
+        return {isValid, errors}
+    } catch (error) {
+        return {isValid: false, errors}
+    }
+}
+
 export const handleSignUpForm = async (prevState: any, formData: FormData) => {
+
+    const validation = validateSignUpForm(formData);
+
+    if(!validation.isValid){
+        return {...prevState, ...validation};
+    }
     
     const data = {
         name: String(formData.get("name")),
@@ -13,5 +73,5 @@ export const handleSignUpForm = async (prevState: any, formData: FormData) => {
     const record = await UsersServices.signUp(data);
     console.log('recotf', record);
 
-    return {}
+    return {...prevState, isValid: true}
 }
