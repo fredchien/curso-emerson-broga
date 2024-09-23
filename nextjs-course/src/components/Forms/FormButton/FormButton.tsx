c9e40f6b6cc70f258edb325e8b160fa2fffcbb87
Frederic Chien <fred_chien@yahoo.com.br>
2024-09-23 08:50:27 -0300

    feat: add logout

---
+++ nextjs-course/src/components/Forms/FormButton/FormButton.tsx
@@ -0,0 +1,17 @@
+export type FormButtonProps = React.ComponentProps<'button'> & {
+    label?: string | null | undefined;
+    action?: () => void;
+}
+
+export default function FormButton({label, children, action, className, ...props}: FormButtonProps){
+    return (
+        <form action={action} className="flex gap-2 items-center rounded-lg p-2 w-full">
+            <button 
+                type="submit" 
+                {...props}
+                className={`block ${className}`}>
+                {label || children}
+            </button>
+        </form>
+    );
+}
